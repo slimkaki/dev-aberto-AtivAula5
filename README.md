52106c7887ea7b7e6a7a5bf55bf0b24bf529e116
slimkaki <rafaalmada98@gmail.com>
2022-09-06 10:46:06 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# My first pip package
+
+Hello World
---
+++ dev_aberto/dev_aberto.py
@@ -0,0 +1,7 @@
+import requests
+
+def hello():
+    c = requests.get('https://api.github.com/repos/insper/dev-aberto/commits')
+    info = c.json()
+    commit_info = info[0]['commit']['author']
+    return commit_info['date'], commit_info['name']

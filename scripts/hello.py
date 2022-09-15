52efdab5ea967fab9dadcae3ff0890872eb36504
slimkaki <rafaalmada98@gmail.com>
2022-09-15 11:23:54 -0300

    adding usefull line commented in case API doesn't return expected data

--- scripts/hello.py
+++ scripts/hello.py
@@ -7,5 +7,6 @@ import gettext
 
 if __name__ == '__main__':
     gettext.install('cli', localedir='locale') 
+    # date, name = '2022-09-12T20:10:47Z', "Rafael"
     date, name = hello()
     print(_('Last commit made in:'), datetime.strftime(datetime.strptime(date, '%Y-%m-%dT%H:%M:%SZ'), "%d/%m/%Y %H:%M:%S"), _(' by'), name)
